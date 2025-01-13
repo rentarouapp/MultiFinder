065f187c20a82a59cf111031594ac6e764a7bdb0
uejo <rentarou1994@gmail.com>
2025-01-13 11:05:37 +0900

    :tada: Initial Commit!!

---
+++ MultiFinder/MultiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiFinderApp.swift
+//  MultiFinder
+//
+//  Created by 上條蓮太朗 on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct MultiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
